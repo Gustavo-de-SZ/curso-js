b25c69146472212514e879f8bcd95f0e6116760a
Gustavo de Souza <072069@PROWAY.TREINA>
2025-08-04 21:55:07 -0300

    aula

---
+++ js/exemplo-01-conversoes.js
@@ -0,0 +1,11 @@
+function converStringParaInt(){
+    const anoFabricacaoEntradaUsuario = "2024";
+    const anoFabricacao = parseInt(anoFabricacaoEntradaUsuario);
+    alert("Ano Fabricacao: "+ anoFabricacao)
+}
+function exemploConverterStringParaFloat(){
+    const precoDoCarro = "299800.10";
+    const preco = parseFloat(precoDoCarro);
+    alert ("Preço" + preco)
+}
+exemploConverterStringParaFloat();
