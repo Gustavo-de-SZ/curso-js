313be2aa6020ac607d0c84217b71a9015469bc42
Gustavo de Souza <072069@PROWAY.TREINA>
2025-08-06 21:52:59 -0300

    aula

---
+++ js/exemplo03-banco.js
@@ -0,0 +1,37 @@
+let saldo = 2000.00;
+
+function sacar() {
+    const valorSaque = parseFloat(
+        prompt("Digite o valor do saque", "R$ ")
+        .replace(",", ".")
+        .replace("R$", "")
+        .replaceAll(" ", "") 
+    );
+
+    if(valorSaque > saldo) {
+        alert("Saldo insuficiente");
+        } else {
+            const confirmacao = confirm("Deseja realmente realizar o saque?")
+            if (confirmacao === true) {
+                saldo = saldo -valorSaque;
+                apresentarSaldo()
+    }
+}
+}
+function apresentarSaldo(){
+    alert("Saldo atual: R$" + saldo.toFixed(2).replace(".",","));
+}
+function depositar() {
+    const valorDeposito = parseFloat(
+        prompt("Digite o valor do depósito", "R$")
+        .replace(",", ".")
+        .replace("R$", "")
+        .replaceAll(" ", "") 
+    );
+    if (valorDeposito <=0) {
+        alert("Valor do depósito não pode ser menor ou igual a zero");
+        depositar();
+        }else {
+            saldo = saldo + valorDeposito;
+    }
+}
---
+++ js/exemplo03-if.js
@@ -0,0 +1,8 @@
+function exemploCarro(){
+    let placa = "ACB-2101";
+    let ano = 2026;
+    let anoFabricacao =2025;
+    let modelo = "BYD Song Plus Premium";
+    let preco = "2998.00"
+}
+
