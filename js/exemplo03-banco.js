91201701f756946427974638e2db43ca8f0a7975
Gustavo de Souza <072069@PROWAY.TREINA>
2025-08-15 21:50:21 -0300

    aula

--- js/exemplo03-banco.js
+++ js/exemplo03-banco.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let saldo = 2000.00;
 
 function sacar() {
@@ -36,47 +35,3 @@ function depositar() {
             saldo = saldo + valorDeposito;
     }
 }
-=======
-// escopo da variável saldo é global
-let saldo = 2000.00;
-
-function sacar() {
-    // escopo da variável valorSaque é a função sacar
-    const valorSaque = parseFloat(
-        prompt("Digite o valor do saque", "R$ ")
-            .replace(",", ".")
-            .replace("R$", "")
-            .replaceAll(" ", "")
-    );
-
-    if (valorSaque > saldo) {
-        alert("Saldo insuficiente");
-    } else {
-        const confirmacao = confirm("Deseja realmente realizar o saque?");
-        if (confirmacao === true) {
-            saldo = saldo - valorSaque;
-            apresentarSaldo();
-        }
-    }
-}
-
-function apresentarSaldo() {
-    alert("Saldo atual: R$ " + saldo.toFixed(2).replace(".", ","));
-}
-
-function depositar() {
-    const valorDeposito = parseFloat(
-        prompt("Digite o valor do depósito", "R$ ")
-            .replace(",", ".")
-            .replace("R$", "")
-            .replaceAll(" ", "")
-    );
-
-    if (valorDeposito <= 0) {
-        alert("Valor do depósito não pode ser menor ou igual a R$ 0,00");
-        depositar(); // recursão, método que chama ele mesmo
-    } else {
-        saldo = saldo + valorDeposito;
-    }
-}
->>>>>>> ab3a5ab0cbb2922fdee44238d9ed5b58a7bf883f
